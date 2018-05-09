a0fa299a1d253a295e79566ea786723982ce4eb1
Winston <bjdhjj@gmail.com>
2018-05-09 11:17:58 +0800

    init

---
+++ v2ray/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ -n "$UUID" ];then
+    sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
+    echo "You've set an UUID"
+    echo "The UUID is: $UUID"
+else
+    export UUID=$(uuidgen)
+    sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
+    echo "You didn't set an UUID"
+    echo "The auto-generated UUID is: $UUID"
+fi
+nginx
+v2ray -config=/etc/v2ray/config.json
