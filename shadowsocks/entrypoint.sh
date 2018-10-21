3a9f64595880cf21e2d37c790db92d5bc93ef8c5
Winston <bjdhjj@gmail.com>
2018-10-21 20:33:36 +0800

    permit passwd config

--- shadowsocks/entrypoint.sh
+++ shadowsocks/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+if [ -n "$PASSWORD" ];then
+    sed -i "s/herosocks/$PASSWORD/g" /etc/v2ray/config.json
+    echo "You've set a password"
+    echo "The password is: $PASSWORD"
+else
+    echo "You didn't set a password"
+    echo "Use default password: tryopenshift"
+fi
 #sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/default.conf
 mkdir -p /var/tmp/nginx/client_body
 mkdir /run/nginx
